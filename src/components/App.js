bbb9cc2d97bc51535034b959175928578117be68
Reshav ji <116823969+Reshavji@users.noreply.github.com>
2023-04-19 22:16:30 +0530

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import '../styles/App.css';
+
+const data = {
+  '2018': [
+    'Avengers 1',
+    'Now you see me',
+    'Fast and Furious 7'
+  ],
+  '2019': [
+    'Avengers 2',
+    'Now you see me 2',
+    'Fast and Furious 8'
+  ],
+  '2020': [
+    'Final Avengers Movie(Iron man dies)',
+    'Now you finally used Lenskart',
+    'Covid Came'
+  ],
+  '2021': [
+    'Covid Returns',
+  ],
+  '2022': [
+    'Adventures of Saiman',
+    'Adventures of Shaktiman'
+  ]
+}
+const App = () => {
+
+  return (
+    <div id="main">
+      
+    </div>
+  )
+}
+
+
+export default App;
